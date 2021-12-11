5a1cd4cb7e44d1c4ac61227842343fbd0c109f84
cbdscolin <cbdscolin@gmail.com>
2021-12-10 23:38:42 -0500

    remove active measurement

--- dns/active_measure.py
+++ dns/active_measure.py
@@ -28,50 +28,53 @@ class LoadMeasurer:
         self.next_load_check_time = time.time() + self.load_check_interval_seconds
 
     def check_load(self):
-        replica_ip_str = ""
-        # Run scamper command to probe replica IPs
-        for ip in self.geo_ip_locator.replica_IPs:
-            replica_ip_str += " -i " + ip
-        cmd = os.popen('timeout 7s scamper -c "trace -d 40002 -P TCP "' + replica_ip_str)
-        out = cmd.read()
-        cmd.close()
-        # Parse scamper output
-        ip_logs = out.split("traceroute")
         replica_ip_ratings_pairs = {}
-        for single_ip_log in ip_logs:
-            single_ip_log = single_ip_log.strip()
-            if single_ip_log == "":
-                continue
-            ip_log_lines = single_ip_log.split("\n")
-            # Parse replica ip
-            replica_ip = ip_log_lines[0].split("to")[1].strip()
-            ratings = 0
-            if len(ip_log_lines) > 0:
-                # last hop has star which means the replica was unreachable.
-                if "*" in ip_log_lines[len(ip_log_lines) - 1]:
-                    ratings = -2
-                else:
-                    # a star in one of the hops means the replica was reachable but some packets were probably lost.
-                    for single_ip_log_line in ip_log_lines:
-                        if "*" in single_ip_log_line:
-                            ratings = -1
-                            break
+        for rep_index, rep_ip in enumerate(self.geo_ip_locator.replica_IPs):
+            replica_ip_ratings_pairs[rep_ip] = 0
+        # replica_ip_str = ""
+        # # Run scamper command to probe replica IPs
+        # for ip in self.geo_ip_locator.replica_IPs:
+        #     replica_ip_str += " -i " + ip
+        # cmd = os.popen('timeout 3s scamper -c "trace -d 40002 -P TCP "' + replica_ip_str)
+        # out = cmd.read()
+        # cmd.close()
+        # # Parse scamper output
+        # ip_logs = out.split("traceroute")
+        # replica_ip_ratings_pairs = {}
+        # for single_ip_log in ip_logs:
+        #     single_ip_log = single_ip_log.strip()
+        #     if single_ip_log == "":
+        #         continue
+        #     ip_log_lines = single_ip_log.split("\n")
+        #     # Parse replica ip
+        #     replica_ip = ip_log_lines[0].split("to")[1].strip()
+        #     ratings = 0
+        #     if len(ip_log_lines) > 0:
+        #         # last hop has star which means the replica was unreachable.
+        #         if "*" in ip_log_lines[len(ip_log_lines) - 1]:
+        #             ratings = -2
+        #         else:
+        #             # a star in one of the hops means the replica was reachable but some packets were probably lost.
+        #             for single_ip_log_line in ip_log_lines:
+        #                 if "*" in single_ip_log_line:
+        #                     ratings = -1
+        #                     break
 
-            replica_ip_ratings_pairs[replica_ip] = ratings
+            # replica_ip_ratings_pairs[replica_ip] = ratings
 
         # Handle the case when scamper output has missing information about some replicas. This might have happened due
         # to timeout.
-        for rep_index, rep_id in enumerate(self.geo_ip_locator.replica_IPs):
-            if rep_id not in replica_ip_ratings_pairs:
-                # If there is no distance information between dns and replica then assume the replica is working
-                if rep_index >= len(self.geo_ip_locator.distance_to_replicas):
-                    replica_ip_ratings_pairs[rep_id] = 0
-                # If the replica is far away from the dns server then assume its up and running
-                elif self.geo_ip_locator.distance_to_replicas[rep_index] > 12000:
-                    replica_ip_ratings_pairs[rep_id] = 0
-                # If the replica is close by and there is no output in scamper assume its down.
-                else:
-                    replica_ip_ratings_pairs[rep_id] = -2
+        # for rep_index, rep_id in enumerate(self.geo_ip_locator.replica_IPs):
+        #     if rep_id not in replica_ip_ratings_pairs:
+        #         # If there is no distance information between dns and replica then assume the replica is working
+        #         if rep_index >= len(self.geo_ip_locator.distance_to_replicas):
+        #             replica_ip_ratings_pairs[rep_id] = 0
+        #         # If the replica is far away from the dns server then assume its up and running
+        #         elif self.geo_ip_locator.distance_to_replicas[rep_index] > 7000:
+        #             replica_ip_ratings_pairs[rep_id] = 0
+        #         # If the replica is close by and there is no output in scamper assume its down.
+        #         else:
+        #             replica_ip_ratings_pairs[rep_id] = -2
 
         # Check load again after this time
         self.next_load_check_time = time.time() + self.load_check_interval_seconds
